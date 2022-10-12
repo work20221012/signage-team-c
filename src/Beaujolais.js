b5deb98f110dac477bc1f020a078cd2daaa7e478
Shingo Sakoda <aeonibs2022-016@iniad.org>
2022-10-12 12:52:31 +0900

    ボジョレー

---
+++ src/Beaujolais.js
@@ -0,0 +1,69 @@
+import './Beaujolais.css'
+const Beaujolais = () => {
+
+return(
+<div style={{padding:20}}>
+
+    <h2><img className="mx-auto d-block" src="https://www.aeondewine.com/img/shop/A106/img/special/c061615/220826-item-01.jpg" alt="早特最大15%OFF+ボーナスポイント100" /></h2>
+    <h2><img className="mx-auto d-block"src="https://www.aeondewine.com/img/shop/A106/img/special/c061615/220826-item-02.jpg" alt="ボージョレーヌーボー 2022ご予約承り中" /></h2>
+    <h3><img className="mx-auto d-block"src="https://www.aeondewine.com/img/shop/A106/img/special/c061615/220826-item-03.jpg" alt="予約8月26日(金)～11月13日(日) お届け日11月17日(木)解禁！" /></h3>
+
+    <ul>
+
+            <div class="cat">
+                <img  src="https://www.aeondewine.com/img/shop/A106/img/special/c061615/220826-item-04.jpg" alt="AEONおとくE予約" width="380" />
+                <div class="qr">
+                    <img src="https://appliv-domestic.akamaized.net/v1/900x/r/articles/118517/86585_1572403072_052203200_0_198_198.png" alt="こちらをクリック" width="50" height="50" />
+                </div>
+            </div>
+
+
+            <div class="cat">
+                <img src="https://www.aeondewine.com/img/shop/A106/img/special/c061615/220826-item-05.jpg" alt="おうちでイオン" width="380" />
+                <div class="qr">
+                    <img src="https://appliv-domestic.akamaized.net/v1/900x/r/articles/118517/86585_1572403072_052203200_0_198_198.png" alt="こちらをクリック" width="50" height="50" />
+                </div>
+            </div>
+
+    </ul>
+
+
+
+</div>
+
+
+
+)
+}
+
+export default Beaujolais;
+/*
+<div style={{padding:20}}>
+
+    <h2><img src="https://www.aeondewine.com/img/shop/A106/img/special/c061615/220826-item-01.jpg" alt="早特最大15%OFF+ボーナスポイント100" /></h2>
+    <h2><img src="https://www.aeondewine.com/img/shop/A106/img/special/c061615/220826-item-02.jpg" alt="ボージョレーヌーボー 2022ご予約承り中" /></h2>
+    <h3><img src="https://www.aeondewine.com/img/shop/A106/img/special/c061615/220826-item-03.jpg" alt="予約8月26日(金)～11月13日(日) お届け日11月17日(木)解禁！" /></h3>
+
+    <ul>
+
+            <div class="cat">
+                <img src="https://www.aeondewine.com/img/shop/A106/img/special/c061615/220826-item-04.jpg" alt="AEONおとくE予約" width="380" />
+                <div class="youtube">
+                <img src="https://appliv-domestic.akamaized.net/v1/900x/r/articles/118517/86585_1572403072_052203200_0_198_198.png" alt="こちらをクリック" width="50" height="50" />
+                </div>
+            </div>
+
+
+            <div class="cat">
+                <img src="https://www.aeondewine.com/img/shop/A106/img/special/c061615/220826-item-05.jpg" alt="おうちでイオン" width="380" />
+                <div class="youtube">
+                    <img src="https://appliv-domestic.akamaized.net/v1/900x/r/articles/118517/86585_1572403072_052203200_0_198_198.png" alt="こちらをクリック" width="50" height="50" />
+                </div>
+            </div>
+
+    </ul>
+
+
+
+</div>
+*/
