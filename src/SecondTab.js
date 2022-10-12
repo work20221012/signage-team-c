124c76416c9b4477674755c5832bca3250015a43
YoshikazuHoshi <aeonibs2022-012@iniad.org>
2022-10-12 13:36:05 +0900

    Tab2にボジョレー実装

--- src/SecondTab.js
+++ src/SecondTab.js
@@ -1,11 +1,34 @@
+import { useEffect } from 'react';
+import Beaujolais from './Beaujolais';
+import NewsViewer from './NewsViewer';
 
 const SecondTab = () => {
   const tabStyle = {
     fontSize: 40,
     backgroundColor: 'pink'
   }
+
+  useEffect(() => {
+    const carousel = new window.bootstrap.Carousel(document.querySelector('.carousel'))
+    carousel.cycle()
+  }, [])
+
   return (
-    <div style={tabStyle}>Second Tab</div>
+    <div style={tabStyle}>
+      <div className="carousel slide" data-bs-ride="carousel" data-bs-interval="2000" data-bs-slide="carousel">
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <Beaujolais />
+          </div>
+          <div className="carousel-item">
+            <Beaujolais />
+          </div>
+          <div className="carousel-item">
+            <Beaujolais />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
